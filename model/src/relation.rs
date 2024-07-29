988d2b09a31a9374929a61296aa26034872eacf1
Umran Hussain <umran.hussein@gmail.com>
2024-07-28 19:05:24 -0700

    created preliminary api to represent many-to-many relations

--- model/src/relation.rs
+++ model/src/relation.rs
@@ -1,33 +1,34 @@
 use crate::{FieldDefinition, Model};
 
-pub struct RelationDef {
-    pub identity: RelationIdent,
-    pub target: RelationTarget,
-}
-
-pub enum RelationIdent {
-    BelongsTo { name: String, reference: String },
-    HasOne { name: String, reference: String },
-    HasMany { name: String, reference: String },
-}
-
-pub struct RelationTarget {
-    pub table_name: fn() -> String,
-    pub id_field_name: fn() -> String,
-    pub field_definitions: fn() -> Vec<FieldDefinition>,
-}
-
 pub trait Relation<T: Model> {
     fn identity() -> RelationIdent;
 
     fn def() -> RelationDef {
         RelationDef {
             identity: Self::identity(),
-            target: RelationTarget {
+            related_model: RelatedModel {
                 table_name: T::table_name,
                 id_field_name: T::id_field_name,
                 field_definitions: T::field_definitions,
             },
         }
     }
+}
+
+pub struct RelationDef {
+    pub identity: RelationIdent,
+    pub related_model: RelatedModel,
+}
+
+pub enum RelationIdent {
+    BelongsTo { name: String, from: String },
+    HasOne { name: String, to: String },
+    HasMany { name: String, to: String },
+    HasManyVia { name: String, via: String, from: String, to: String }
+}
+
+pub struct RelatedModel {
+    pub table_name: fn() -> String,
+    pub id_field_name: fn() -> String,
+    pub field_definitions: fn() -> Vec<FieldDefinition>,
 }
