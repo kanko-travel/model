7dd9c32fb04ba1714a862f4acce7881270f1b961
Umran Hussain <umran.hussein@gmail.com>
2024-08-13 19:58:30 -0700

    introduced api to create and delte associations

---
+++ model/src/crud/delete_association.rs
@@ -0,0 +1,64 @@
+use sqlx::{postgres::PgRow, FromRow, PgConnection};
+use uuid::Uuid;
+
+use crate::relation::Reference;
+use crate::Error;
+use crate::{crud::util::build_query, FieldValue, Model};
+
+#[derive(Clone, Debug)]
+pub struct DeleteAssociation<'a, T: Model> {
+    value: &'a T,
+    relation_name: &'a str,
+    associated_id: &'a Uuid,
+}
+
+impl<'a, T> DeleteAssociation<'a, T>
+where
+    T: Model + for<'b> FromRow<'b, PgRow> + Unpin + Sized + Send,
+{
+    pub(crate) fn new(value: &'a T, relation_name: &'a str, associated_id: &'a Uuid) -> Self {
+        Self {
+            value,
+            relation_name,
+            associated_id,
+        }
+    }
+
+    pub async fn execute(&self, executor: &mut PgConnection) -> Result<(), Error> {
+        let relation_defs = T::relation_definitions();
+
+        let relation = relation_defs
+            .into_iter()
+            .find(|rel| rel.name == self.relation_name)
+            .ok_or_else(|| {
+                Error::bad_request(
+                    "invalid relation: can't create association for non-existent relation",
+                )
+            })?;
+
+        match relation.reference {
+            Reference::Via((junction_table, from_ref, to_ref)) => {
+                let statement = format!(
+                    "DELETE FROM {} WHERE {} = $1 AND {} = $2",
+                    junction_table, from_ref, to_ref
+                );
+
+                let var_bindings: Vec<FieldValue> = vec![
+                    self.value.id_field_value().into(),
+                    self.associated_id.clone().into(),
+                ];
+
+                build_query(&statement, var_bindings)
+                    .execute(executor)
+                    .await?;
+
+                Ok(())
+            }
+            _ => {
+                return Err(Error::bad_request(
+                    "create_association can only be used with many-to-many relations",
+                ))
+            }
+        }
+    }
+}
--- model/src/crud/mod.rs
+++ model/src/crud/mod.rs
@@ -1,4 +1,14 @@
+mod create;
+mod create_association;
+mod delete;
+mod delete_association;
+mod select;
+mod update;
+mod util;
+
 use async_trait::async_trait;
+use create_association::CreateAssociation;
+use delete_association::DeleteAssociation;
 use sqlx::{Database, FromRow, Postgres};
 use uuid::Uuid;
 
@@ -59,6 +69,22 @@ where
         Ok(Select::new().with_filter(filter))
     }
 
+    fn create_association<'a>(
+        &'a self,
+        relation_name: &'a str,
+        associated_id: &'a Uuid,
+    ) -> CreateAssociation<'a, Self> {
+        CreateAssociation::new(self, relation_name, associated_id)
+    }
+
+    fn delete_association<'a>(
+        &'a self,
+        relation_name: &'a str,
+        associated_id: &'a Uuid,
+    ) -> DeleteAssociation<'a, Self> {
+        DeleteAssociation::new(self, relation_name, associated_id)
+    }
+
     fn create<'a>(&'a self) -> Create<'a, Self> {
         Create::new(self)
     }
@@ -90,9 +116,3 @@ impl<T> Crud for T where
         + Send
 {
 }
-
-mod create;
-mod delete;
-mod select;
-mod update;
-mod util;
